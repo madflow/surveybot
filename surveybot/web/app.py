d81b3cd7316d8b5ae1ba551aa463beb30e2473e7
madflow <madflow@localhost>
2014-01-16 21:40:13 +0100

    Hacking

--- surveybot/web/app.py
+++ surveybot/web/app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
