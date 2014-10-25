da1edc4b979755802636e15ecb0a4ca00347fa48
madflow <madflow@localhost>
2014-10-25 16:17:49 +0200

    WIP

--- surveybot/bot.py
+++ surveybot/bot.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Bot Working Horse"""
 import random
+
 from splinter import Browser
 from splinter.exceptions import ElementDoesNotExist
 
@@ -25,8 +26,19 @@ class SurveyBot(object):
         browser = Browser()
         browser.visit(self.url)
 
+        try:
+            while browser.find_by_tag('button').first:
+                self.process_elements(browser)
+                browser.find_by_tag('button').first.click()
+        except ElementDoesNotExist:
+            pass
+
+    def process_elements(self, browser):
         for field in browser.find_by_xpath('//input[@type="text"]'):
             self.process_text(field)
+
+        for password_field in browser.find_by_xpath('//input[@type="password"]'):
+            self.process_text(password_field)
 
         for checkbox in browser.find_by_xpath('//input[@type="checkbox"]'):
             self.process_textbox(checkbox)
@@ -34,11 +46,8 @@ class SurveyBot(object):
         for textarea in browser.find_by_tag('textarea'):
             self.process_text(textarea)
 
-        try:
-            while browser.find_by_tag('button').first:
-                browser.find_by_tag('button').first.click()
-        except ElementDoesNotExist:
-            pass
+        for select in browser.find_by_tag('select'):
+            self.process_select(select)
 
     def process_text(self, el):
         """Process input[type="text"] and textareas"""
@@ -53,6 +62,17 @@ class SurveyBot(object):
             random.shuffle(self.random_text)
             el.value = self.random_text[0]
         except:
+            pass
+
+    def process_select(self, el):
+        if self.varmap is None:
+            options = el.find_by_tag('option')
+            if el['multiple']:
+                for i in range(1,len(options)):
+                    random.choice(options).click()
+            else:
+                random.choice(options).click()
+        else:
             pass
 
     def process_textbox(self, el):
